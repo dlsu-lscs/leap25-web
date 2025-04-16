5fcbd48ad305e1e22d19f4535d13c6d7a03431b0
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-16 18:27:25 +0800

    feat: finish class card

--- src/features/classCard/ClassCard.tsx
+++ src/features/classCard/ClassCard.tsx
@@ -12,7 +12,7 @@ export default function ClassCard() {
           <div className="h-[500px] w-[500px] bg-[#D9D9D9]"></div>
         </div>
         <div>
-          <div className="space-x-3 text-sm">
+          <div className="space-x-3 text-sm font-medium">
             <Button text="S1137"></Button>
             <Button text="Sub-Theme"></Button>
             <Button text="Tag Here"></Button>
@@ -35,6 +35,45 @@ export default function ClassCard() {
           </div>
           <div className="my-4 w-[70vh]">
             <ClassDescription></ClassDescription>
+          </div>
+          <div className="my-6 flex justify-between">
+            <div className="flex items-center space-x-3">
+              <Button
+                text="Join Now - Free"
+                className="bg-[#ADADAD] px-4 py-2 font-medium"
+              ></Button>
+              <p className="text-shadow-lg">Only {22} slots left!</p>
+            </div>
+            <div className="flex items-center space-x-4.5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-7"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-7"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
+                />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
