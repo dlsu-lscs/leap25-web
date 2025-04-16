aca336db63469e91d9b61b5b73dfae22361dc670
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-17 03:06:07 +0800

    feat: add title page for maps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 'use client';
 
-export default function Home() {
+import LeapTag from '@/components/ui/LeapTag';
+
+export default function Map() {
   return (
     <>
-      <p>hello this is the first step of the leap web</p>
+      <div className="absolute left-1/2 z-20 -translate-x-1/2 flex justify-center">
+        <div
+          className="
+        flex flex-col items-center py-8
+        "
+        >
+          <div className="my-3 space-x-3 text-xs">
+            <LeapTag>Tag Badge Here</LeapTag>
+            <LeapTag>Tag Badge Here</LeapTag>
+            <LeapTag>Tag Badge Here</LeapTag>
+          </div>
+          <div>
+            <h1 className="text-4xl font-bold">Leap into the Adventure</h1>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-100 min-h-screen"></div>
     </>
   );
 }
