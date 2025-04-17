fb07afb131370d02b6e8a71b86a2ec83d1183412
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-17 15:35:44 +0800

    feat: add sub theme carousel component (no carousel only test text)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import LeapTag from '@/components/ui/LeapTag';
+import SubThemeCarousel from '@/features/MapSubTheme/SubThemeCarousel';
 
 export default function Map() {
   return (
@@ -11,17 +12,26 @@ export default function Map() {
         flex flex-col items-center py-8
         "
         >
-          <div className="my-3 space-x-3 text-xs">
+          <div className="hidden md:inline my-6 space-x-3 text-sm">
             <LeapTag>Tag Badge Here</LeapTag>
             <LeapTag>Tag Badge Here</LeapTag>
+            <LeapTag>Tag Badge Here</LeapTag>
+          </div>
+          <div className="md:hidden inline my-3 space-x-3 text-xs">
             <LeapTag>Tag Badge Here</LeapTag>
           </div>
           <div>
-            <h1 className="text-4xl font-bold">Leap into the Adventure</h1>
+            <h1 className="md:text-5xl text-4xl mx-5 font-bold max-w-[16ch]">
+              Leap into the Adventure
+            </h1>
           </div>
         </div>
       </div>
-      <div className="min-h-screen"></div>
+      <div className="min-h-screen">
+        <div className="md:hidden inline">
+          <SubThemeCarousel></SubThemeCarousel>
+        </div>
+      </div>
     </>
   );
 }
