d73d1f8389d21820393e90ec3ce179150063d84c
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-18 13:41:55 +0800

    feat: change classhighlight parameter of class title to children accepting a react node

--- src/features/subthemeComponents/subthemeClassHighlightCard.tsx
+++ src/features/subthemeComponents/subthemeClassHighlightCard.tsx
@@ -4,16 +4,18 @@
  * @returns
  */
 
+import { ReactNode } from 'react';
+
 interface classHighlightProps {
   imgLink: string;
-  classTitle: string;
+  children: ReactNode;
 }
 
-export default function ClassHighlight({ imgLink, classTitle }: classHighlightProps) {
+export default function ClassHighlight({ imgLink, children }: classHighlightProps) {
   return (
     <div>
       <img src={imgLink || undefined} className="h-[512px] w-[410px]" />
-      <h1 className="text-3xl font-semibold w-[410px] mt-4">{classTitle}</h1>
+      <h1 className="text-3xl font-semibold w-[410px] mt-4">{children}</h1>
     </div>
   );
 }
