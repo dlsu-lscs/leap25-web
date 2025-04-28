fcdd178a35182d3c341dc3ef1d0a7ee1dab1c35b
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-28 13:12:14 +0800

    feat: add a history.back() for back icon

--- src/features/classCard/ClassNavbar.tsx
+++ src/features/classCard/ClassNavbar.tsx
@@ -13,9 +13,18 @@ interface ClassNavbarProps {
 export default function ClassNavbar({ subtheme, className, src }: ClassNavbarProps) {
   return (
     <>
-      <div className={`bg-black text-white min-w-screen flex justify-between p-6  ${className}`}>
+      <div
+        className={`bg-black/75 bg-opacity-50 text-white min-w-screen flex justify-between px-8 py-5  ${className}`}
+      >
         <div className="flex items-center text-2xl space-x-1.5">
-          <ChevronLeftOutlinedIcon sx={{ fontSize: 48 }}></ChevronLeftOutlinedIcon>
+          <div
+            onClick={() => {
+              history.back();
+            }}
+            className="hover:opacity-50 transition duration-50"
+          >
+            <ChevronLeftOutlinedIcon sx={{ fontSize: 48 }}></ChevronLeftOutlinedIcon>
+          </div>
           <div>{subtheme || 'Sub Theme'}</div>
           <Avatar className="w-8 h-8 mx-2 text-xs">
             <AvatarImage src={src || undefined} />
