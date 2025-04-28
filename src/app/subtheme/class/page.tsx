22dee3b813c683f85e120295893343216a19c86a
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-28 12:46:42 +0800

    feat: replicate class page with figma

--- src/app/subtheme/class/page.tsx
+++ src/app/subtheme/class/page.tsx
@@ -4,27 +4,35 @@ import ClassCard from '@/features/classCard/ClassCard';
 import ClassNavbar from '@/features/classCard/ClassNavbar';
 import { subThemeModel } from '@/types/classModels';
 
+import { Public_Sans } from 'next/font/google';
+
+const public_sans = Public_Sans({ subsets: ['latin'] });
+
 export default function Class({ title }: subThemeModel) {
   return (
     <>
       <div className="fixed top-0 z-20">
         <ClassNavbar></ClassNavbar>
       </div>
-      <div className={`flex flex-col p-40 py-44 bg-gray-200`}>
+      <div
+        className={`flex flex-col p-40 py-44 bg-[url(/subthemeBG/Lost_Boys_Hideout_BG.png)]  bg-black/40 bg-blend-multiply bg-cover`}
+      >
         <div>
           <ClassCard></ClassCard>
         </div>
-        <div className="my-20 space-y-8">
+        <div
+          className={`my-20 space-y-8 text-white ${public_sans.className} font-semibold text-4xl text-shadow-lg`}
+        >
           <div className="space-y-5">
-            <p className="font-medium text-3xl">More in {title || 'SubTheme'}</p>
+            <p>More in {title || 'SubTheme'}</p>
             <LeapCarousel row2={false} itemsToShow={Array.from({ length: 8 })}></LeapCarousel>
           </div>
           <div className="space-y-5">
-            <p className="font-medium text-3xl">If you liked this, you might like...</p>
+            <p>If you liked this, you might like...</p>
             <LeapCarousel row2={false} itemsToShow={Array.from({ length: 8 })}></LeapCarousel>
           </div>
           <div className="space-y-5">
-            <p className="font-medium text-3xl">Recently Viewed</p>
+            <p>Recently Viewed</p>
             <LeapCarousel row2={false} itemsToShow={Array.from({ length: 8 })}></LeapCarousel>
           </div>
         </div>
