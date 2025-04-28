fe63e87f03f0c98a21d8078703d897ebf34e0221
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-28 11:56:26 +0800

    feat: add sub theme logo to navbar

--- src/features/classCard/ClassNavbar.tsx
+++ src/features/classCard/ClassNavbar.tsx
@@ -1,19 +1,26 @@
 'use client';
 import { ChevronLeftIcon } from '@heroicons/react/24/solid';
 import { UserIcon } from '@heroicons/react/24/outline';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { nameInitials } from '@/lib/helpers';
 
 interface ClassNavbarProps {
   subtheme: string;
   className?: string;
+  src: string;
 }
 
-export default function ClassNavbar({ subtheme, className }: ClassNavbarProps) {
+export default function ClassNavbar({ subtheme, className, src }: ClassNavbarProps) {
   return (
     <>
       <div className={`bg-black text-white min-w-screen flex justify-between p-6  ${className}`}>
         <div className="flex items-center text-2xl space-x-1.5">
           <ChevronLeftIcon className="w-10 h-10"></ChevronLeftIcon>
           <div>{subtheme || 'Sub Theme'}</div>
+          <Avatar className="w-8 h-8 mx-2 text-xs">
+            <AvatarImage src={src || undefined} />
+            <AvatarFallback>{nameInitials(subtheme || 'na')}</AvatarFallback>
+          </Avatar>
         </div>
         <div>
           <UserIcon className="w-10 h-10"></UserIcon>
