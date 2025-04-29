2833173a97adedc9c3c0ac000b861a40baedd682
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-30 01:29:15 +0800

    fix: add diamond varaint

--- src/components/ui/LeapSeperator.tsx
+++ src/components/ui/LeapSeperator.tsx
@@ -1,9 +1,26 @@
-export default function LeapSeperator() {
+interface LeapSeperatorProps {
+  variant?: string;
+}
+
+/**
+ * @param variants Variants Available: none, diamond, circle
+ * @returns
+ */
+export default function LeapSeperator({ variant }: LeapSeperatorProps) {
   return (
     <>
       <div className="flex items-center">
-        <div className="bg-white w-2 h-2 rounded-full"></div>
-        <div className="bg-gradient-to-r flex-1 from-white to-transparent h-[0.2vh] w-[100vh]"></div>
+        {variant == 'diamond' ? (
+          <>
+            <div className=" absolute -translate-x-3 w-4 h-4 border-white border-2 rotate-45"></div>
+            <div className=" w-3 h-3   border-white border-2 rotate-45"></div>
+          </>
+        ) : (
+          <>
+            <div className="bg-white w-2 h-2 rounded-full"></div>
+          </>
+        )}
+        <div className="bg-gradient-to-r from-white to-transparent h-[0.2vh] w-[100vh]"></div>
       </div>
     </>
   );
