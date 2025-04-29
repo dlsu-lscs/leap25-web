d73e7d5474ec71d8389bd811b3a6ee9cbd5ed039
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-04-29 11:49:40 +0800

    fix: changed the behavior of passed in items to the LeapCarousel

--- src/app/subtheme/page.tsx
+++ src/app/subtheme/page.tsx
@@ -23,23 +23,29 @@ export default function Subtheme() {
 
   return (
     <>
-      <div className="min-h-screen py-12 px-24 bg-gray-400 text-white">
+      <div className="min-h-screen sm:py-12 sm:px-24 bg-gray-400 text-white">
         <div className="flex items-center">
           <h1 className={`text-[64px] font-bold ${playfair_display.className}`}>LEAP's choice</h1>
         </div>
       </div>
-      <div className="min-h-screen py-12 px-24 bg-gray-800 text-white">
+      <div className="min-h-screen sm:py-12 sm:px-24 bg-gray-800 text-white">
         <div>
-          <h1 className={`text-[64px] font-bold ${playfair_display.className}`}>
+          <h1
+            className={`text-4xl sm:text-[64px] font-bold text-center sm:text-justify ${playfair_display.className}`}
+          >
             The Pirate’s Cove
           </h1>
           <div className="py-4 space-y-4">
             <div>
-              <h2 className={`text-[30px] font-bold ${playfair_display.className}`}>Day 1</h2>
+              <h2 className={`text-[30px] font-bold sm:ml-0 ml-4 ${playfair_display.className}`}>
+                Day 1
+              </h2>
             </div>
             <LeapCarousel loopItems={false} row2={false} itemsToShow={dummyData}></LeapCarousel>
             <div>
-              <h2 className={`text-[30px] font-bold ${playfair_display.className}`}>Day 2</h2>
+              <h2 className={`text-[30px] font-bold sm:ml-0 ml- ${playfair_display.className}`}>
+                Day 2
+              </h2>
             </div>
             <LeapCarousel loopItems={false} row2={false} itemsToShow={dummyData}></LeapCarousel>
           </div>
