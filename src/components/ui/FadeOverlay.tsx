b397ef1a4dcdd8122c69efe262e87f7a1c148145
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-04-30 22:42:39 +0900

    feat: add fade overlay

--- src/components/ui/FadeOverlay.tsx
+++ src/components/ui/FadeOverlay.tsx
@@ -1,7 +1,7 @@
 export default function FadeOverlay() {
   return (
     <>
-      <div className="bg-black min-w-screen h-24 blur-lg opacity-80"></div>
+      <div className="bg-black min-w-screen h-28 blur-xl opacity-90"></div>
     </>
   );
 }
