f068d60ebbe3adfc9b570ce41f887a446f1af8d1
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-05-03 22:39:32 +0800

    fix: make login mobile friendly

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -9,13 +9,13 @@ const playfair_display_sc_regular = Playfair_Display_SC({ subsets: ['latin'], we
 export default function Login() {
   return (
     <>
-      <div className="flex flex-col h-full w-full justify-center items-center bg-[url('/landingBG.png')] bg-center bg-cover bg-black/50 bg-blend-multiply">
+      <div className="flex flex-col h-full w-full justify-center items-center bg-[url('/landingBG.png')] bg-center bg-cover bg-black/50 bg-blend-multiply text-center">
         <div className="-translate-y-20">
           <div className={`flex flex-col items-center justify-center space-y-3 `}>
             <div className="h-[300px] overflow-hidden">
               <Image
                 src="/leapLogos/Logo_v1.png"
-                width={429}
+                width={419}
                 height={113}
                 className="object-cover"
                 style={{ objectPosition: 'center' }}
@@ -23,16 +23,17 @@ export default function Login() {
               />
             </div>
             <h1
-              className={`font-bold text-[48px] text-[#FBBC05] ${playfair_display_sc_bold.className} text-shadow-lg inset-shadow-lg`}
+              className={`text-center font-bold text-[30px] sm:text-[48px] text-[#FBBC05] ${playfair_display_sc_bold.className} text-shadow-lg inset-shadow-lg`}
             >
-              WELCOME TO <span className="text-[#98C10E]"> NEVERLAND</span>
+              WELCOME TO
+              <span className="text-[#98C10E]"> NEVERLAND</span>
             </h1>
             <h2
-              className={`text-white text-[20px] ${playfair_display_sc_regular.className} text-shadow-lg inset-shadow-lg`}
+              className={`text-center text-white text-[16px] sm:text-[20px] ${playfair_display_sc_regular.className} text-shadow-lg inset-shadow-lg`}
             >
               STEP INTO NEVERLAND - THE ADVENTURE AWAITS.
             </h2>
-            <div className="font-light text-[15px] text-white text-shadow-lg inset-shadow-lg">
+            <div className="font-light text-[12px] sm:text-[15px] text-white text-shadow-lg inset-shadow-lg">
               <p className="flex items-center justify-center">
                 Powered by{' '}
                 <Image
@@ -47,7 +48,7 @@ export default function Login() {
             </div>
           </div>
           <div className="flex flex-col items-center justify-center space-y-3 py-16">
-            <p className="text-white text-[20px] font-light text-shadow-lg inset-shadow-lg">
+            <p className="text-white text-[16px] sm:text-[20px] font-light text-shadow-lg inset-shadow-lg">
               Use your DLSU Gmail to unlock your portal.
             </p>
             <GoogleLogin></GoogleLogin>
