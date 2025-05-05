cdbd67477342019a07eafd7f6ebca5b8baa3f5fa
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-05-06 00:57:02 +0800

    feat: add animation and hitbox for desktop map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { Playfair_Display_SC } from 'next/font/google';
 import MapButton from '@/components/ui/MapButton';
 import { useState } from 'react';
+import SubThemeMapPicker from '@/features/MapSubTheme/SubThemeMapPicker';
 
 const playfair_display_sc_bold = Playfair_Display_SC({ subsets: ['latin'], weight: '700' });
 
@@ -16,11 +17,12 @@ export default function Map() {
   const [isStart, setStart] = useState(false);
   return (
     <>
-      <div className="flex justify-between w-full h-full flex-col bg-[url('/map/LEAP_MAP.webp')] bg-black/60 bg-blend-multiply bg-cover bg-center">
+      <div className="flex justify-between w-full h-full flex-col bg-[url('/map/LEAP_MAP.webp')] bg-black/40 bg-blend-multiply bg-cover bg-center">
         <AnimatePresence>
           {isStart ? (
             <>
               <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,_transparent_30%,_rgba(0,10,30,0.45)_100%)]  pointer-events-none z-10"></div>
+              <SubThemeMapPicker></SubThemeMapPicker>
             </>
           ) : (
             <>
