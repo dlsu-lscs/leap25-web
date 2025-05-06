8e92090e4d64c4812c477972d35c02664abb8199
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-05-06 11:22:13 +0800

    feat: add landmark

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { Playfair_Display_SC } from 'next/font/google';
 import MapButton from '@/components/ui/MapButton';
 import { useState } from 'react';
-import SubThemeMapPicker from '@/features/MapSubTheme/SubThemeMapPicker';
+import SubThemeMapPicker from '@/features/MapSubTheme/DesktopMapSubTheme/SubThemeMapPicker';
 
 const playfair_display_sc_bold = Playfair_Display_SC({ subsets: ['latin'], weight: '700' });
 
@@ -22,7 +22,9 @@ export default function Map() {
           {isStart ? (
             <>
               <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,_transparent_30%,_rgba(0,10,30,0.45)_100%)]  pointer-events-none z-10"></div>
-              <SubThemeMapPicker></SubThemeMapPicker>
+              <div className="fixed z-20">
+                <SubThemeMapPicker></SubThemeMapPicker>
+              </div>
             </>
           ) : (
             <>
