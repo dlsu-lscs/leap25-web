01a2055d4a62223831aa38cd3fb6ecb8d6f10535
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-05-11 20:20:58 +0800

    Revert "feat: movable map (#84)" (#85)
    This reverts commit 00b07468c5689ec14d6b98e2bd9ab1c10a4f74f9.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`h-full font-public-sans`}>
       <link rel="icon" href="/leapLogos/Logo_v1.png" />
-      <body className="h-full overflow-x-hidden">
+      <body className="h-full">
         <Providers>
           {children}
           <LeapCursor />
