35dd81b0e880facb8821e44d8990507cf13c688e
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-05-12 17:53:14 +0800

    fix: minor ui

--- src/app/[subtheme]/page.tsx
+++ src/app/[subtheme]/page.tsx
@@ -42,7 +42,9 @@ export default async function Subtheme({ params }: { params: Promise<{ subtheme:
                   <AvatarFallback>{nameInitials('na')}</AvatarFallback>
                 </Avatar>
                 <div className="flex gap-4 justify-center">
-                  <h1 className={`text-3xl md:text-6xl font-bold whitespace-nowrap font-playfair `}>
+                  <h1
+                    className={`text-3xl md:text-6xl font-bold sm:text-nowrap text-wrap font-playfair `}
+                  >
                     {subthemeDetails.title}
                   </h1>
                   <LeapSeperator direction="right"></LeapSeperator>
