e1fde27cf7ebfca5bc4933c450851b07f7451af8
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-05-12 15:08:04 +0800

    feat: make text also a link in navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -25,20 +25,23 @@ export default function Navbar({ className, src, name }: NavbarProps) {
         className={`bg-black/0.2 text-white min-w-screen flex justify-between px-8 py-5  ${className}`}
       >
         <div className="flex items-center text-2xl space-x-1.5">
-          <a href="./" className="hover:opacity-50 transition duration-50">
+          <a
+            href="./"
+            className="flex items-center space-x-1 hover:opacity-50 transition duration-50"
+          >
             <ChevronLeftOutlinedIcon sx={{ fontSize: 48 }} role="button"></ChevronLeftOutlinedIcon>
+            {name == undefined ? (
+              <div className={`text-[20px] ${public_sans.className}`}>Back to Map</div>
+            ) : (
+              <>
+                <div className={`text-[20px] ${public_sans.className}`}>{name}</div>
+                <Avatar className="w-9 h-9 mx-2 text-xs">
+                  <AvatarImage src={src} />
+                  <AvatarFallback>{nameInitials(name || 'na')}</AvatarFallback>
+                </Avatar>
+              </>
+            )}
           </a>
-          {name == undefined ? (
-            <div className={`text-[20px] ${public_sans.className}`}>Back to Map</div>
-          ) : (
-            <>
-              <div className={`text-[20px] ${public_sans.className}`}>{name}</div>
-              <Avatar className="w-9 h-9 mx-2 text-xs">
-                <AvatarImage src={src} />
-                <AvatarFallback>{nameInitials(name || 'na')}</AvatarFallback>
-              </Avatar>
-            </>
-          )}
         </div>
         <div>
           <Avatar className="w-10 h-10 text-xs text-black">
