22740bb56f9643ddbec5586d6aaedf6716ade3df
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-05-14 21:27:49 +0800

    feat: add accordion

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,12 +1,98 @@
 import Navbar from '@/components/layout/Navbar';
 import AuthRedirectProvider from '@/context/authRedirectProvider';
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+
+const dummyData = [
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do?',
+    answer:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do?',
+    answer:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do?',
+    answer:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do?',
+    answer:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do?',
+    answer:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do?',
+    answer:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do?',
+    answer:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do?',
+    answer:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do?',
+    answer:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do?',
+    answer:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+];
+
 export default function FAQ() {
   return (
     <>
       <AuthRedirectProvider>
         <div className="fixed top-0 z-20">
           <Navbar />
+        </div>
+        <div
+          className={`min-h-screen bg-black/60 bg-blend-multiply bg-cover bg-center 
+            bg-[url('/landingBG.webp')] flex flex-col justify-center items-center font-public-sans font-medium text-white
+            `}
+        >
+          <div
+            className="flex flex-col justify-center items-center
+          font-bold tracking-wide text-shadow-xl inset-shadow-xl font-playfair text-[48px] 
+          "
+          >
+            <h1 className="text-[#FBBC05]">Frequently Asked</h1>
+            <h1 className="text-[#98C10E]">Questions</h1>
+          </div>
+          <div className="my-12">
+            <Accordion type="single" className="space-y-3" collapsible>
+              {dummyData.map((question, index) => {
+                return (
+                  <AccordionItem key={index} value={`item-${index}`}>
+                    <AccordionTrigger className="text-[24px]">{question.question}</AccordionTrigger>
+                    <AccordionContent>{question.answer}</AccordionContent>
+                  </AccordionItem>
+                );
+              })}
+            </Accordion>
+          </div>
         </div>
       </AuthRedirectProvider>
     </>
