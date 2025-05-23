16f423c69885f2d8434c119e2ac65911a7477f5b
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-05-24 02:37:26 +0800

    chore: fixed go back

--- src/app/lscs/page.tsx
+++ src/app/lscs/page.tsx
@@ -8,7 +8,7 @@ export default function AboutUs() {
   return (
     <>
       <div className="sm:bg-black/0 bg-black/20 fixed top-0 z-20">
-        <Navbar />
+        <Navbar variant="non-map" />
       </div>
       <div
         className={`min-h-screen bg-black/60 bg-blend-multiply bg-cover bg-center 
