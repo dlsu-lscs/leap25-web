123df4e6bff0ad6cb6133e4151dafbdce1e3e75a
Max Chavez <epikstrykermaxchavez23@gmail.com>
2025-05-24 02:38:05 +0800

    Chore/go back (#217)
    * chore: change subtheme title to desc
    
    * chore: fixed go back

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -47,11 +47,12 @@ interface NavbarProps {
   name?: string;
   src?: string;
   className?: string;
+  variant?: string;
 }
 
 const public_sans = Public_Sans({ subsets: ['latin'] });
 
-export default function Navbar({ className, src, name }: NavbarProps) {
+export default function Navbar({ className, src, name, variant }: NavbarProps) {
   const { data: session, status } = useSession();
   const router = useRouter();
   const [showSearch, setShowSearch] = useState(false);
@@ -59,7 +60,6 @@ export default function Navbar({ className, src, name }: NavbarProps) {
   const [showMobileSearchResults, setShowMobileSearchResults] = useState(false);
   const [searchValue, setSearchValue] = useState('');
   const { event: searchedEvents } = useSetSearchEvent(searchValue);
-  console.log(searchedEvents);
 
   useEffect(() => {
     if (status === 'unauthenticated') {
@@ -67,39 +67,46 @@ export default function Navbar({ className, src, name }: NavbarProps) {
     }
   }, [status, router]);
 
+  console.log(name);
+
   return (
     <div
       className={`bg-black/0.2 text-white min-w-screen flex justify-between sm:px-8 px-4 py-5  ${className}`}
     >
       <div className="flex items-center text-2xl space-x-1.5">
-        <a
-          href="./"
-          className="flex items-center space-x-1 hover:opacity-50 transition duration-50"
-        >
-          <ChevronLeftOutlinedIcon sx={{ fontSize: 48 }} role="button" />
-          {name == undefined ? (
-            <div className={`sm:text-[20px] text-base ${public_sans.className}`}>Back to Map</div>
-          ) : (
-            <>
-              {' '}
-              <p className={`sm:text-[20px] text-base ${public_sans.className}`}>{name}</p>
-              <div
-                className="w-10 h-10 mx-2 hidden sm:block rounded-full p-[2px]"
-                style={{
-                  background:
-                    'linear-gradient(to right, #A67C00, #B38B18, #FFBF00, #FFCF40, #FFDC73)',
-                }}
-              >
-                <Avatar className="w-full h-full text-xs">
-                  <AvatarImage src={src} className="rounded-full" />
-                  <AvatarFallback className="bg-black text-[#FBBC05] rounded-full">
-                    {nameInitials(name || 'na')}
-                  </AvatarFallback>
-                </Avatar>
-              </div>
-            </>
-          )}
-        </a>
+        {name === undefined ? (
+          <a
+            onClick={() => router.back()}
+            className="flex items-center space-x-1 hover:opacity-50 transition duration-50"
+          >
+            <ChevronLeftOutlinedIcon sx={{ fontSize: 48 }} role="button" />
+            <div className={`sm:text-[20px] text-base ${public_sans.className}`}>
+              {variant === 'non-map' ? 'Go Back' : 'Back to Map'}
+            </div>
+          </a>
+        ) : (
+          <a
+            href="./"
+            className="flex items-center space-x-1 hover:opacity-50 transition duration-50"
+          >
+            <ChevronLeftOutlinedIcon sx={{ fontSize: 48 }} role="button" />
+            <p className={`sm:text-[20px] text-base ${public_sans.className}`}>{name}</p>
+            <div
+              className="w-10 h-10 mx-2 hidden sm:block rounded-full p-[2px]"
+              style={{
+                background:
+                  'linear-gradient(to right, #A67C00, #B38B18, #FFBF00, #FFCF40, #FFDC73)',
+              }}
+            >
+              <Avatar className="w-full h-full text-xs">
+                <AvatarImage src={src} className="rounded-full" />
+                <AvatarFallback className="bg-black text-[#FBBC05] rounded-full">
+                  {nameInitials(name)}
+                </AvatarFallback>
+              </Avatar>
+            </div>
+          </a>
+        )}
       </div>{' '}
       <div className="flex items-center gap-4">
         <div className="hidden sm:inline">
