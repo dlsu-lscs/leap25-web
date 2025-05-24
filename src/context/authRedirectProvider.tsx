e9e38c2fa1a6f51d0a0592d5cb9f7abdcc2076e9
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-05-24 18:36:28 +0800

    chore: removed auth redirect

--- src/context/authRedirectProvider.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import useGoogleAuthRedirect from '@/hooks/useGoogleAuthRedirect';
-import { ReactNode } from 'react';
-
-export default function AuthRedirectProvider({ children }: { children: ReactNode }) {
-  useGoogleAuthRedirect();
-  return <>{children}</>;
-}
