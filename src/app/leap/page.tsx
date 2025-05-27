ea044a4c59f3482389a5c9df36762d746ab1105c
Max Chavez <epikstrykermaxchavez23@gmail.com>
2025-05-27 19:26:28 +0800

    feat: added info for about-us (#276)

--- src/app/leap/page.tsx
+++ src/app/leap/page.tsx
@@ -9,58 +9,52 @@ export default function AboutUs() {
       </div>
       <div
         className={`min-h-screen bg-black/60 bg-blend-multiply bg-cover bg-center 
-            bg-[url('/landingBG.webp')] flex flex-col justify-center items-center font-public-sans font-medium text-white
+            bg-[url('/landingBG.webp')] flex flex-col items-center font-public-sans font-medium text-white 
             `}
       >
-        <div
-          className="flex flex-col justify-center items-center
+        <div className="flex flex-col items-center -translate-y-8">
+          <div
+            className="flex flex-col justify-center items-center
           font-bold tracking-wide text-shadow-xl inset-shadow-xl font-playfair 
-           text-[32px] sm:text-[52px] text-center 
+           text-[32px] sm:text-[64px] text-center 
           "
-        >
-          <div className="relative h-[250px] sm:h-[350px] overflow-hidden">
-            <Image
-              src="/leapLogos/Logo_v1.png"
-              alt="Leap 2025"
-              width={500}
-              height={300}
-              className="object-cover object-center w-[350px] sm:w-[500px]"
-            />
+          >
+            <div className="relative h-[250px] sm:h-[330px] overflow-hidden">
+              <Image
+                src="/leapLogos/Logo_v1.png"
+                alt="Leap 2025"
+                width={500}
+                height={300}
+                className="object-cover object-center w-[350px] sm:w-[500px]"
+              />
+            </div>
+            <h1>
+              <span className="text-[#FBBC05]">About </span>
+              <span className="text-[#98C10E]">Us</span>
+            </h1>
           </div>
-          <h1>
-            <span className="text-[#FBBC05]">About </span>
-            <span className="text-[#98C10E]">Us</span>
-          </h1>
-        </div>
-        <div className="flex flex-col text-white font-public-sans text-[16px] sm:text-[20px] sm:w-1/2 sm:m-12 p-8">
-          <p className="font-light">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </p>
-          <div className="flex flex-col my-6">
-            <p className="font-bold">Section 1.10.32 of "de Finibus Bonorum et Malorum":</p>
-            <ul>
-              <li>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-                doloremque laudantium,
-              </li>
-              <li>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-                doloremque laudantium,
-              </li>
-              <li>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-                doloremque laudantium,
-              </li>
-              <li>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-                doloremque laudantium,
-              </li>
-            </ul>
+          <div className="flex flex-col text-white justfy-center font-public-sans text-[18px] sm:text-[24px] sm:mx-12 sm:my-6 my-8 px-8 gap-8">
+            <p className="font-light">
+              The Lasallian Enrichment Alternative Program (LEAP) is a week-long celebration of
+              personal growth, creativity, and learning beyond the classroom—proudly spearheaded by
+              the Council of Student Organizations (CSO) of De La Salle University.
+            </p>
+            <p className="font-light">
+              LEAP Week 2025 runs from June 20, 21, 23, 25, and 26, featuring dynamic,
+              interest-driven workshops and sessions led by DLSU’s diverse student organizations. On
+              LEAP Day (June 26), regular academic classes are suspended to give way to a
+              university-wide experience of exploration and engagement. It’s a unique opportunity to
+              discover new passions, develop essential life skills, and connect with the Lasallian
+              community in fun, but meaningful ways.
+            </p>
+            <p className="font-light">
+              This year’s theme, <span className="font-bold">LEAP 2025: Soar Beyond Limits</span>,
+              draws inspiration from adventure and the inner child within us all. It’s about
+              reigniting curiosity, embracing play, and stepping boldly into spaces that challenge,
+              excite, and inspire. Whether you're dancing, crafting, cooking, exploring advocacy, or
+              learning something entirely new, LEAP Week is your playground for growth, connection,
+              and self-discovery.
+            </p>
           </div>
         </div>
       </div>
