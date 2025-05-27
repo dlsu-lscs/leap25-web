791427d21781307f5d813259b428940fd0f011f1
Max Chavez <epikstrykermaxchavez23@gmail.com>
2025-05-27 21:22:37 +0800

    feat: added info for faq (#277)

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -27,7 +27,7 @@ export default function FAQ() {
           <FAQAccordion></FAQAccordion>
         </div>
         <div className="my-8 hidden sm:inline">
-          <ScrollArea className="h-[400px] w-[800px]">
+          <ScrollArea className="h-[500px] w-[800px]">
             <FAQAccordion></FAQAccordion>
           </ScrollArea>
         </div>
---
+++ src/lib/data.tsx
@@ -0,0 +1,50 @@
+export const FAQ_QUESTIONS = [
+  {
+    question: 'What is LEAP?',
+    answer:
+      'LEAP stands for Lasallian Enrichment Alternative Program—a week-long alternative learning initiative where students engage in interest-based workshops. These sessions are designed to promote holistic development in line with De La Salle University’s Lasallian values.',
+  },
+  {
+    question: 'Who can join LEAP?',
+    answer:
+      'All enrolled Senior High School and undergraduate students of DLSU are required to participate in LEAP.',
+  },
+  {
+    question: 'How do I join a session?',
+    answer:
+      'Students must pre-register online through the official LEAP website. Sessions have limited slots and fill up quickly, so early registration is highly encouraged!',
+  },
+  {
+    question: 'Is attendance required?',
+    answer:
+      'Yes. LEAP is a part of DLSU’s formative learning experience, and students are expected to attend at least one session to fulfill participation requirements.',
+  },
+  {
+    question: 'Can I attend more than one session?',
+    answer:
+      'Yes, students are allowed to attend multiple sessions. However, we ask that you be considerate—registering for too many sessions may prevent others from joining. Choose the sessions that truly interest you!',
+  },
+  {
+    question: 'Do LEAP classes have fees?',
+    answer:
+      'Most LEAP classes are free, but select sessions may require a fee to cover materials or other costs. These will be indicated during registration.',
+  },
+  {
+    question: 'Am I allowed to enroll in a LEAP class in Laguna?',
+    answer: (
+      <>
+        <p className="pb-2">
+          Yes, you may register for sessions held in the Laguna Campus. However, please note that
+          transportation will be at your own arrangement.
+        </p>
+        <a
+          className="text-[#FBBC05] font-playfair font-extrabold text-xl hover:scale-110 transition duration-200 ease-in-out text-shadow-lg drop-shadow-lg box-shadow-lg"
+          href="https://dlsucso-leap.com/"
+          target="_blank"
+        >
+          LEAP <span className="text-[#98C10E]">LAGUNA Website</span>
+        </a>
+      </>
+    ),
+  },
+];
