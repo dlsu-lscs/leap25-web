1c0d2c0f43de76e6a8b12aeaca1ce5bf9d5f248e
Max Chavez <epikstrykermaxchavez23@gmail.com>
2025-06-03 14:57:45 +0800

    Chore/update team (#328)
    * chore: updated team (not yet finished)
    
    * chore: updated team (not yet finished)

--- src/features/lscsContent/LSCSMember.tsx
+++ src/features/lscsContent/LSCSMember.tsx
@@ -11,17 +11,17 @@ export default function LSCSMember({
 }) {
   return (
     <>
-      <div className="flex flex-col space-y-3">
-        <div className="bg-gradient-to-b from-[#003D6E] to-[#002B50] rounded-full w-60 h-60 drop-shadow-xl inset-shadow-xl flex justify-center items-center">
+      <div className="flex flex-col items-center justify-center space-y-3.5">
+        <div>
           <img
-            className="-translate-y-12 translate-x-3 hover:-translate-y-6 transition duration-200 ease-in-out w-[600px] h-[350px] "
+            className="hover:-translate-y-6 transition duration-200 ease-in-out w-56 h-auto "
             src={src}
             alt="member"
           />
         </div>
-        <div className="x-col space-y-1">
-          <h1 className="text-white font-playfair text-[16px] sm:text-[24px] font-bold">{name}</h1>
-          <p className="text-[#E9C438] font-public-sans font-light text-[16px] sm:text[24px]">
+        <div className="space-y-1">
+          <h1 className="text-white font-playfair text-[16px] sm:text-[20px] font-bold">{name}</h1>
+          <p className="text-[#E9C438] font-public-sans font-light text-[16px] sm:text[20px]">
             {position}
           </p>
         </div>
