38e82e590287b5b38bbf1dab7d90e427616de870
Max Chavez <epikstrykermaxchavez23@gmail.com>
2025-06-04 10:28:35 +0800

    Update helpers.ts

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -37,10 +37,3 @@ export function formatDate(date: any, durationMinutes = 60) {
 
   return `${startStr}/${endStr}`;
 }
-
-const normalizeImageUrl = (url: string | undefined | null) => {
-  if (!url) return fallBackImage;
-  if (url.startsWith('http')) return url;
-  if (url.startsWith('//')) return `https:${url}`;
-  return `${BASE_URL}${url}`; // fallback for relative paths
-};
