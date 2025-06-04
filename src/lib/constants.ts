859bec8a240c32f6d10a86de61907301e7d1a8d3
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-06-04 20:28:14 +0900

    chore: added hardcoded

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,6 @@
-export const API_URL = process.env.LEAP_API || process.env.NEXT_PUBLIC_LEAP_API;
+export const API_URL =
+  process.env.LEAP_API || process.env.NEXT_PUBLIC_LEAP_API || 'https://api.dlsucso-leap.com';
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://dlsucso-leap.com';
 
 export const API_SECRET = process.env.API_SECRET;
