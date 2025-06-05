691a5fbd7846d678006619ecdff6110cf6498416
Max Chavez <epikstrykermaxchavez23@gmail.com>
2025-06-05 13:20:08 +0800

    Chore/optimize fetch (#399)
    * chore: fixed undefined issues
    
    * chore: fixed undefined issues

--- src/features/bookmark/BookmarkedEvents.tsx
+++ src/features/bookmark/BookmarkedEvents.tsx
@@ -22,48 +22,52 @@ export default function BookmarkedEvents({ event_id, variant }: BookmarkedEvents
   const { eventMedia }: { eventMedia: classPubModel | undefined } = useSetEventMedia(event_id);
   const { subtheme }: { subtheme: subThemeModel | undefined } = useSetSubtheme(event?.subtheme_id);
   const subthemeLink = getSubThemeLink(subtheme?.title);
-
+  console.log(subthemeLink);
   const slotsLeft = (event?.max_slots ?? 0) - (event?.registered_slots ?? 0);
 
   const title = event?.title?.replace(/^LEAP 2025:\s*/i, '') ?? '';
 
   return (
     <>
-      <a
-        href={`/${subthemeLink}/${event?.slug}`}
-        className={
-          variant === 'search'
-            ? 'font-playfair p-4 sm:p-6 rounded-lg bg-cover bg-center  bg-blend-multiply object-cover bg-black/70 hover:bg-black/90 duration-200 transition ease-in-out'
-            : 'font-playfair p-4 sm:p-6 rounded-full bg-cover bg-center  bg-blend-multiply object-cover bg-black/70 hover:bg-black/90 duration-200 transition ease-in-out'
-        }
-        style={{
-          backgroundImage: `url(${eventMedia?.pub_url || 'https://i.imgur.com/Rjo6F4G.png'})`,
-        }}
-      >
-        <div className="flex flex-col items-center justify-center text-center gap-2">
-          <div className="font-semibold text-[16px] sm:text-[24px] break-all">
-            {title || 'Event Title'}
-          </div>
-          <div className="text-[12px] sm:text-[16px] flex justify-between gap-4">
-            <div className="flex items-center gap-1">
-              <PersonIcon />
-              <p>{slotsLeft > 0 ? `${slotsLeft} slots left` : 'Event Full'}</p>
+      {subtheme !== undefined ? (
+        <>
+          <a
+            href={`/${subthemeLink}/${event?.slug}`}
+            className={
+              variant === 'search'
+                ? 'font-playfair p-4 sm:p-6 rounded-lg bg-cover bg-center  bg-blend-multiply object-cover bg-black/70 hover:bg-black/90 duration-200 transition ease-in-out'
+                : 'font-playfair p-4 sm:p-6 rounded-full bg-cover bg-center  bg-blend-multiply object-cover bg-black/70 hover:bg-black/90 duration-200 transition ease-in-out'
+            }
+            style={{
+              backgroundImage: `url(${eventMedia?.pub_url || 'https://i.imgur.com/Rjo6F4G.png'})`,
+            }}
+          >
+            <div className="flex flex-col items-center justify-center text-center gap-2">
+              <div className="font-semibold text-[16px] sm:text-[24px] break-all">
+                {title || 'Event Title'}
+              </div>
+              <div className="text-[12px] sm:text-[16px] flex justify-between gap-4">
+                <div className="flex items-center gap-1">
+                  <PersonIcon />
+                  <p>{slotsLeft > 0 ? `${slotsLeft} slots left` : 'Event Full'}</p>
+                </div>
+                <div className="flex items-center gap-1">
+                  <CalendarViewIcon />
+                  <p>
+                    {' '}
+                    {event?.schedule &&
+                      new Date(event.schedule).toLocaleDateString('en-US', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric',
+                      })}
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="flex items-center gap-1">
-              <CalendarViewIcon />
-              <p>
-                {' '}
-                {event?.schedule &&
-                  new Date(event.schedule).toLocaleDateString('en-US', {
-                    year: 'numeric',
-                    month: 'long',
-                    day: 'numeric',
-                  })}
-              </p>
-            </div>
-          </div>
-        </div>
-      </a>
+          </a>
+        </>
+      ) : null}
     </>
   );
 }
