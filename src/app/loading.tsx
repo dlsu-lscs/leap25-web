b7f80341e31e07902b73f34a13667f36ac119493
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-06-12 13:50:59 +0800

    feat: timeout error page if loading for too long (#414)

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,23 @@
+'use client';
+
 import Image from 'next/image';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Loading() {
+  const router = useRouter();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log('nawiufbawfbwab');
+      // Redirect to timeout error page
+      router.push(
+        '/timeout-error?message=Connection%20timeout%20-%20Please%20check%20your%20internet%20connection'
+      );
+    }, 15000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
     <>
       <div className="h-full w-full bg-[url('/landingBG.webp')] bg-cover bg-center bg-black/50 bg-blend-multiply flex items-center justify-center flex-col">
