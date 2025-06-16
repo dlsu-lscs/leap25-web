e193c7dc468e022f27ae680d67bbf9a140a34d23
Max Chavez <epikstrykermaxchavez23@gmail.com>
2025-06-16 10:07:20 +0800

    chore: fixed issues (#415)

--- src/features/classCard/HostName.tsx
+++ src/features/classCard/HostName.tsx
@@ -9,11 +9,16 @@ interface HostProps {
   org_url?: any;
 }
 export default function HostName({ children, src, org_url }: HostProps) {
+  console.log(org_url);
   return (
     <>
       {org_url ? (
         <>
-          <a href={org_url} target="_blank" className="flex space-x-2 items-cente">
+          <a
+            href={org_url}
+            target="_blank"
+            className="flex space-x-2 items-center cursor-pointer px-2 py-1 rounded-md bg-white/02 hover:bg-white/20 transition duration-200"
+          >
             {src ? (
               <img
                 src={src}
@@ -21,10 +26,24 @@ export default function HostName({ children, src, org_url }: HostProps) {
                 alt="logo"
               />
             ) : (
-              <GroupsIcon sx={{ fontSize: 24, color: 'white' }}></GroupsIcon>
+              <GroupsIcon sx={{ fontSize: 24, color: 'white' }} />
             )}
-            <p className="font-medium text-shadow-lg drop-shadow-amber-300 drop-shadow-xl hover:text-white/60">
+            <p className="font-semibold text-amber-300 text-shadow-lg drop-shadow-md flex items-center space-x-1">
               {children}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.8}
+                stroke="currentColor"
+                className="w-4 h-4 opacity-80"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 4.5H19.5V10.5M19.5 4.5L10.5 13.5M4.5 19.5L13.5 10.5"
+                />
+              </svg>
             </p>
           </a>
         </>
